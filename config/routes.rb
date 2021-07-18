de0c942c8d35299c4f8996ab7f4525ce30c9b781
EC2 Default User <ec2-user@ip-172-31-26-232.ec2.internal>
2021-07-18 04:55:53 +0000

    [Finish]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only:[:create, :destroy]
+    resources :post_comments, only: [:create, :destroy] # post_imagesにcommentするため、doとendで挟んで親子関係にする
+  end
 
 end
